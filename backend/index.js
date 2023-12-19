4dae01ec83d273d741d0d87cde4e96bbdc728fb6
Khushali Vaidya <khushalivaidya0@gmail.com>
2023-12-20 00:11:12 +0530

    fixing bugs

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(
     cors({
         origin: allowedOrigins,
         credentials: true,
-        methods: ["GET", "POST", "PUT"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
     })
 );
 
